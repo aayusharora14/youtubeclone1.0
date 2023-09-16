02a7480ebb469a2d17c5080c038f65834b1f3652
AayushAroraatgithub <127019590+AayushAroraatgithub@users.noreply.github.com>
2023-09-16 18:52:07 +0530

    Update VideoPage.jsx

--- client/src/Pages/VideoPage/VideoPage.jsx
+++ client/src/Pages/VideoPage/VideoPage.jsx
@@ -49,7 +49,7 @@ function VideoPage() {
                     <div className="video_display_screen_videoPage">
                         <video
                             src={`http://localhost:5500/${vv?.filePath}`}
-                            //src={`https://youtubeclone5031.herokuapp.com/${vv?.filePath}`}
+                            //src={`https://youtubeclone1-by-aayush.onrender.com/${vv?.filePath}`}
                             className={"video_ShowVideo_videoPage"}
                             controls
                             autoPlay
